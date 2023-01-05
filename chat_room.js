99394e55350aff8f097efabe7a0bcd66e8cfc1c2
vinodkumarbalusupati <119593574+vinodkumarbalusupati@users.noreply.github.com>
2023-01-05 19:02:53 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAhfn5w5CKd9JjYn1rOHiZytT_xcJlENQQ",
+    authDomain: "lets-chat-web-app-f4967.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-f4967-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-f4967",
+    storageBucket: "lets-chat-web-app-f4967.appspot.com",
+    messagingSenderId: "190534195842",
+    appId: "1:190534195842:web:3c0f7696cf303bc2abf29b",
+    measurementId: "G-12K3JB0GTP"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
